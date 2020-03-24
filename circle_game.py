1d3799b376647726046321f76ea4d7a837c133ac
Quart71 <mrfunny9601@gmail.com>
2020-03-24 19:42:32 +0300

    Finish development

--- circle_game.py
+++ circle_game.py
@@ -2,8 +2,8 @@ import arcade
 import random
 
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1366
+SCREEN_HEIGHT = 768
 SCREEN_TITLE = 'circle_game'
 
 class Circle:
@@ -40,11 +40,8 @@ class Circle:
         self.center_y = side['y']
         self.change_x = side['dx']
         self.change_y = side['dy']
-        self.color = (random.randint(50, 256), random.randint(50, 256), random.randint(50, 256))
-        self.left = self.center_x - self.radius
-        self.right = self.center_x + self.radius
-        self.top = self.center_y + self.radius
-        self.bottom = self.center_y - self.radius
+        self.color = (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255))
+        self.update_sides()
 
     def draw(self):
         arcade.draw_circle_filled(self.center_x, self.center_y, self.radius, self.color)
@@ -52,6 +49,9 @@ class Circle:
     def update(self):
         self.center_x += self.change_x
         self.center_y += self.change_y
+        self.update_sides()
+
+    def update_sides(self):
         self.left = self.center_x - self.radius
         self.right = self.center_x + self.radius
         self.top = self.center_y + self.radius
@@ -68,34 +68,73 @@ class Player(Circle):
 
 class Game(arcade.Window):
     def __init__(self, width, height, title):
-        super().__init__(width, height, title)
+        super().__init__(width, height, title, fullscreen=True)
         arcade.set_background_color(arcade.color.BLACK)
         self.circles = []
         self.player = None
+        self.status = True
+        width, heigth = self.get_size()
+        self.set_viewport(0, width, 0, height)
+        self.left, self.screen_width, self.bottom, self.screen_height = self.get_viewport()
 
     def update(self, delta_time):
-        for circle in self.circles:
-            if circle.right < 0 or circle.left > SCREEN_WIDTH or circle.top < 0 or circle.bottom > SCREEN_HEIGHT:
-                self.replace_circle(circle)
-            circle.update()
+        if self.status:
+            if self.check_for_collision():
+                circle = self.check_for_collision()
+                if self.player.radius > circle.radius:
+                    self.replace_circle(circle)
+                    self.player.radius += 1
+                    if self.player.radius >= 80:
+                        self.status = False
+                else:
+                    self.status = False
+            for circle in self.circles:
+                if circle.right < 0 or circle.left > SCREEN_WIDTH or circle.top < 0 or circle.bottom > SCREEN_HEIGHT:
+                    self.replace_circle(circle)
+                circle.update()
+
+
 
     def setup(self):
-        for i in range(10):
-            self.circles.append(Circle(random.randint(5, 10)))
         self.player = Player()
+        for i in range(10):
+            self.circles.append(Circle(random.randint(5, self.player.radius+40)))
 
 
     def on_draw(self):
         arcade.start_render()
+        self.player.draw()
         for circle in self.circles:
             circle.draw()
-        self.player.draw()
+
 
     def replace_circle(self, circle):
         # узнаем номер шарика в списке
         index = self.circles.index(circle)
         self.circles.pop(index)
-        self.circles.append(Circle(random.randint(5, 10)))
+        self.circles.append(Circle(random.randint(5, self.player.radius+40)))
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        if self.status:
+            self.player.center_x = x
+            self.player.center_y = y
+
+    def check_for_collision(self):
+        for circle in self.circles:
+            distance = self.player.radius + circle.radius
+            if self.player.center_x > circle.center_x and self.player.center_y > circle.center_y:
+                if self.player.center_x - circle.center_x <= distance and self.player.center_y - circle.center_y <= distance:
+                    return circle
+            elif self.player.center_x > circle.center_x and self.player.center_y <= circle.center_y:
+                if self.player.center_x - circle.center_x <= distance and circle.center_y - self.player.center_y <= distance:
+                    return circle
+            elif self.player.center_x <= circle.center_x and self.player.center_y > circle.center_y:
+                if circle.center_x - self.player.center_x <= distance and self.player.center_y - circle.center_y <= distance:
+                    return circle
+            elif self.player.center_x <= circle.center_x and self.player.center_y <= circle.center_y:
+                if circle.center_x - self.player.center_x <= distance and circle.center_y - self.player.center_y <= distance:
+                    return circle
+
 
 window = Game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 window.setup()
